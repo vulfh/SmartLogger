61ffce6cd4e2c2f55441ec99824a9c0a3c01195d
vulfh <Miyamoto1977!>
2024-05-13 10:57:35 +0300

    Added configuration

---
+++ SmartLogger/Configuration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartLogger;
+
+public class Configuration
+{
+    /// <summary>
+    /// Format of TimeStamp in log message. If empty, timestamp will not be added to the message
+    /// </summary>
+    public string TimeStampFormat { get; private set; } = Constants.General.DefaultTimeStampFormat;
+
+    /// <summary>
+    /// Flag to indicate whether add the source line number, where logging method was called from
+    /// </summary>
+    public bool SourceLineNumber { get; private set; } = true;
+
+    /// <summary>
+    /// Flag to indicate whether add the method name, where logging method was called from
+    /// </summary>
+    public bool CallingMethodName { get; private set; } = true;
+
+    /// <summary>
+    /// Flag to indicate whether add the source file path, where logging method was called from
+    /// </summary>
+    public bool SourceFilePath { get; private set; } = true;
+
+    /// <summary>
+    /// Flag to indicate whether add log timestamp
+    /// </summary>
+    public bool LogTimeStamp { get; private set; } = true;
+
+
+}
---
+++ SmartLogger/SmartLogger.cs
@@ -0,0 +1,79 @@
+﻿using SmartLogger.LogPersistance;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartLogger;
+
+public class SmartLogger : ILogAggregator
+{
+
+    private ConcurrentBag<LogMessage> _messages = new ConcurrentBag<LogMessage>();
+
+    private Configuration _configuration = new Configuration();
+
+    #region ILogAggregator
+    void ILogAggregator.Flush(Severity? severity)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task ILogAggregator.FlushAsync(Severity? severity)
+    {
+        throw new NotImplementedException();
+    }
+
+    void ILogAggregator.LogDebug(string message, int lineNumber, string memberName, string filePath)
+    {
+        throw new NotImplementedException();
+    }
+
+    void ILogAggregator.LogError(string message, int lineNumber, string memberName, string filePath)
+    {
+        throw new NotImplementedException();
+    }
+
+    void ILogAggregator.LogError(Exception exception, int lineNumber, string memberName, string filePath)
+    {
+        throw new NotImplementedException();
+    }
+
+    void ILogAggregator.LogFatal(string message, int lineNumber, string memberName, string filePath)
+    {
+        throw new NotImplementedException();
+    }
+
+    void ILogAggregator.LogFatal(Exception exception, int lineNumber, string memberName, string filePath)
+    {
+        throw new NotImplementedException();
+    }
+
+    void ILogAggregator.LogInformation(string message, int lineNumber, string memberName, string filePath)
+    {
+        throw new NotImplementedException();
+    }
+
+    void ILogAggregator.LogWarning(string message, int lineNumber, string memberName, string filePath)
+    {
+        throw new NotImplementedException();
+    }
+
+    bool ILogAggregator.RegisterObserver(string name, Action<LogMessage> observer)
+    {
+        throw new NotImplementedException();
+    }
+
+    void ILogAggregator.SetLogPersistanceSeverity(Severity severity)
+    {
+        throw new NotImplementedException();
+    }
+
+    bool ILogAggregator.UnregisterObserver(string name)
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
+}
