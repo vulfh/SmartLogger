e0823cb7ded7e0898347fd43e120bccc05092a05
vulfh <vulfhm@gmail.com>
2024-05-13 11:09:09 +0300

    Added configuration (#1)
    Co-authored-by: vulfh <Miyamoto1977!>

--- SmartLogger/Constants.cs
+++ SmartLogger/Constants.cs
@@ -11,6 +11,7 @@ namespace SmartLogger
         public class General
         {
             public const  string NotAssigned = "N/A";
+            public const string DefaultTimeStampFormat = "yyyy-MM-dd HH:mm:ss.fff tt";
         }
     }
 }
