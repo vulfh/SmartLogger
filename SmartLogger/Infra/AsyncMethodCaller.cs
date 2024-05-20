10018251bfa1100639df00933ff5570b920db1e5
vulfh <vulfhm@gmail.com>
2024-05-20 22:48:05 +0300

    FlushAsync implemented (#5)
    Co-authored-by: vulfh <Miyamoto1977!>

--- SmartLogger/Infra/AsyncMethodCaller.cs
+++ SmartLogger/Infra/AsyncMethodCaller.cs
@@ -3,3 +3,4 @@ namespace SmartLogger.Infra;
 
 public delegate Output AsyncMethodCaller<Input,Output>(Input inputParam);
 public delegate void AsyncMethodCaller<Input>(Input inputParam);
+public delegate void AsyncMethodCaller();
