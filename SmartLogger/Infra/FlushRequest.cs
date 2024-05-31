202d86490fa9a6013fcb5a61f950fb73f39015c2
vulfh <Miyamoto1977!>
2024-05-31 12:38:32 +0300

    Added flush request Added IDisposable Refactores flush queue

---
+++ SmartLogger/Infra/FlushRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartLogger.Infra;
+
+internal record FlushRequest
+{
+    public DateTime TimeStamp { get; private set; } = DateTime.UtcNow;
+    public Severity Severity { get; private init; }
+
+    public FlushRequest(Severity severity) {  this.Severity = severity; }
+}
